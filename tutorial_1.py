da52fb52c843717288297386c892b4c481b06945
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2022-07-18 20:51:27 +0100

    Create tutorial_1.py

---
+++ tutorial_1.py
@@ -0,0 +1,31 @@
+#1. Randomly select an element from a list
+
+#2. Generate a random number in 0/1
+
+#3. Use random in 0/1 to mask 50% of the words in a string
+
+#4. Use random in 0/1 to mask 70% of the words in a string
+
+#5. Turn a string into capital/lowercase letters
+
+#6. Get an input from the user using the default input() method
+
+#7. Check if an element is in a list/dictionary using the 'in' statement
+
+#8. Remove an element from a list/dictionary
+
+#9. Load in memory the content of a text file
+
+#10. The use of enumerate() in a for loop
+
+#11. Define a function that returns more than one element (introduce the idea of "tuple")
+
+# Exercise
+# - take a random word as string e.g. "umbrella"
+# - Iterate over all the characters in the string with a for-loop (with enumerate)
+# - Use the character as key of a dictionary
+# - For each key add a list as value that contains the positions/index
+#     of that character in the string.
+#
+# e.g. for the word "umbrella" the dictionary should be: 
+# {'u': [0], 'm': [1], 'b': [2], 'r': [3], 'e': [4], 'l': [5, 6], 'a': [7]}
