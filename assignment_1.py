1ba92b71ef7bb09198f5fec4d6095492c785d789
Massimiliano Patacchiola <massimiliano.patacchiola@gmail.com>
2022-07-21 16:13:02 +0100

    Removing previous empty functions (confusing) and providing a function for get_masked_word()

--- assignment_1.py
+++ assignment_1.py
@@ -1,68 +1,69 @@
 import random
 
-def load_words_list(file_name):
-    ''' Function to load a text-file content in memory
-    input: the name of a file containint the words
-    output: a list containing all the words
-    '''
-
-def get_random_word(words_list):
-    ''' Function to get a random word from a list of words
-    input: a list of words
-    output: one of the words in the list (random choice)
-    '''
-
 def get_masked_word(word):
     ''' Function to get a masked word from a word (drops ~50% of letters)
+    e.g. "CAVIAR" -> "C-V--R"; "UNICORN" -> "U-IC-R-"
     input: the word as a string
-    output: two things, the masked word (as a string) and a
-      dictionary of masked words with their position in the word.
+    output: the masked word (as a string) and a
+      dictionary of masked letters with their position in the word.
+    Example: get_masked_word("UNICORN") can return the tuple
+    ('U-IC---', {'N': [1, 6], 'O': [4], 'R': [5]})
     '''
-    #1. Define an empty dictionary for storing the dropped letters
-    # This dictionary will use the letter as key, and a list of positions as value.
-    # e.g. {"a": [1,3], "d": 2} means that the letter a is in position 1 and 3 in the word.
-    
-    #2. Iterate on the input word with a for-loop
-    # Per each letter trow a coin (random number included in [0, 1], 50%)
-    
-    #3. If the random number is less than 0.5 keep the word,
-    # if it is higher that 0.5 mask the letter and add its position in the dictionary.
-    
-    #4. When the for loop is finished return the masked word
-    # and the dictionary of masked letters.
+    # Get unique letters in the word
+    unique_letters_list = list(set(word)) # set() finds unique elements in string
+    # Shuffling the list of unique letters
+    random.shuffle(unique_letters_list)
+    # Selecting just 50% of the letters
+    split_point = len(unique_letters_list) // 2
+    letters_to_mask_list = unique_letters_list[0:split_point]
+    # Creating the masked word and the dictionary
+    masked_word = ""
+    masked_letter_position_dict = {}
+    for letter_position, letter in enumerate(word):
+        if(letter in letters_to_mask_list):
+            masked_word = masked_word + "-"
+            # We store the position of the masked letter in a dictionary
+            if(letter in masked_letter_position_dict):
+                # If the letter is in the dictionary we append the position
+                masked_letter_position_dict[letter].append(letter_position)
+            else:
+                # If the letter is NOT in the dictionary we create the entry (a list)
+                masked_letter_position_dict[letter] = [letter_position]
+        else:
+            masked_word = masked_word + letter        
+    # Finished, let's return the masked letters and the
+    # dictionary containinng the position of the masked letters.
+    return masked_word, masked_letter_position_dict
 
 
 #0. Define a variable for the game score, e.g. score=3
 # where 3 is the maximum number of attempts for guessing the word (score==0 the user lost)
 
 #1. Load all the available words in memory (by reading them from a text file, or hard-coding them)
-# You can encapsulate this piece of code in the function called load_words_list() above
 
 #2. Pick a random word from all the available words.
-# You can encapsulate this code into the function called get_random_word() above.
 
 #3. Turn the letters in the word into capital letters, search for the default method .upper()
 
-#4. Produce a masked version of the word (hide some letters).
-# Write the code for the function get_masked_word().
-# For example if the word is PENCIL the function get_masked_word() 
-# returns something like P-N-I- or -EN-I- and a dictionary of the masked letters.
-# The dictionary will use the masked letter as key, and a list of positions as value.
-# e.g. {"a": [1,3], "d": 2} means that the letter "a" is in position 1 and 3 in the word.
+#4. Use the function get_masked_word().
+# This function returns the masked word, and a dictionary.
+# The dictionary contains the masked letters as keys,
+# and a list of their positions as values.
+# e.g. if you call the function in this way: get_masked_word("UNICORN")
+# the result could be: ('U-IC---', {'N': [1, 6], 'O': [4], 'R': [5]})
 
 #5. Print on screen the masked word and ask the user to 
 # type a letter in the terminal, you can use the default method input()
 
 #6. You need to check if the user provided more than one letter.
-# Consider just the first character and discard the rest.
+# If this is the case, take just the first character and discard the rest.
 
 #7. Turn the character provided by the user into a capital letter
 
 #8. Check if the letter provided by the user is a good one or a bad one.
-# - First, check if the letter is already visible (the user did a mistake), decrease the score.
-# - Second, if the letter is in the dictionary of masked letters, the user did a good choice 
-#     (now you need to make those letters visible and remove the letter from the dictionary).
-# - Third, the letter is neither in the visible letters, nor in the masked letters, decrease the score.
+# Recall: in step-4 you used the function get_masked_word() to get a dictionary of masked letters.
+# If the letter is in the dictionary then it's a GOOD one (show this letter in masked_word)
+# If the letter is NOT in the dictionary it's a BAD one (decrease the score)
 
 #9. Print the current user score, the masked word, and the list of letters already tried.
-# If the score==0 the game is finished, a new match can start (score=3)
+# If the score==0 the game is finished, a new match can start (set again score=3)
